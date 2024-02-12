2d25d928a3a9163c47886b9bc90f66186390eabc
malasiaa <jose.jn@hotmail.com>
2024-02-12 16:55:21 +0000

    Main code - not accessing .h5 file

--- AGG_WebApp.py
+++ AGG_WebApp.py
@@ -23,7 +23,6 @@ def f1_m(y_true, y_pred):
     recall = recall_m(y_true, y_pred)
     return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
-st.image('charlie_eatingmachine.gif')
 # Load the model
 #model = tf.keras.models.load_model('vgg_foodclass.h5', custom_objects={"f1_m": f1_m})
 
