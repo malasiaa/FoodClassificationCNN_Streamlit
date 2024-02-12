652f00b68d7afd4ff32e780d0b5028274fcd0c0b
malasiaa <jose.jn@hotmail.com>
2024-02-12 14:54:45 +0000

    Main code

--- AGG_WebApp.py
+++ AGG_WebApp.py
@@ -25,6 +25,7 @@ def f1_m(y_true, y_pred):
 
 # Load the model
 os.chdir(os.getcwd())
+st.write(print(os.getcwd()))
 model=tf.keras.models.load_model('vgg_foodclass.h5', custom_objects={"f1_m": f1_m})
 
 st.set_page_config(
