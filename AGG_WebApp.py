d1c282d43edf3177777f94863913780c5a102326
malasiaa <jose.jn@hotmail.com>
2024-02-24 17:39:21 +0000

    Update AGG_WebApp.py

--- AGG_WebApp.py
+++ AGG_WebApp.py
@@ -96,23 +96,24 @@ file = st.file_uploader("**Please upload a food image:**", type=["jpg","png","jp
 if file is None:
     st.text("")
 else:
-    try:
-        # If a file is uploaded, open it as an image
-        image=Image.open(file)
-        # The page width was divided into three for no particular reason besides looks
-        cols = st.columns(3)
-        # Display the uploaded image in the first column with fixed width
-        with cols[0]:
-            st.image(image, width=400, use_column_width=False)
-        # Predict the class of the uploaded image using the imported_and_predict function
-        with cols[1]:
+    # If a file is uploaded, open it as an image
+    image=Image.open(file)
+    # The page width was divided into three for no particular reason besides looks
+    cols = st.columns(3)
+    # Display the uploaded image in the first column with fixed width
+    with cols[0]:
+        st.image(image, width=400, use_column_width=False)
+    # Predict the class of the uploaded image using the imported_and_predict function
+    with cols[1]:
+        try:
             predictions = import_and_predict(image, model)
             class_names = ['Baked Potato', 'Crispy Chicken', 'Donut', 'Fries', 'Hot Dog', 'Sandwich', 'a Taco!', 'Taquitooo :)', 'Apple Pie', 'a Burguer', 'Butter Naan', 'Chai', 'Chapati', 'Cheesecake', 'Chicken Curry', 'Chole Bhatura', 'Dal Makhani', 'Dhokla', 'Fried Rice', 'Ice Cream', 'Idli', 'Jalebi', 'Kaathi Rolls', 'Kadai Paneer', 'Kulfi', 'Masala Dosa', 'Momos', 'Omelette', 'Paani Puri', 'Pakode', 'Pav Bhaji', 'Pizzaaaaa!', 'Samosa', 'Sushi! (disguised sugars)']
             string="This image most likely is: "+class_names[np.argmax(predictions)]
             st.success(string)
-        with cols[2]:
-            st.write(' ')
-    except:
-        st.markdown('EEUnable to categorize image, please try another one! :)\n(try by searching for the categories above in google**)')
+        except: 
+            st.markdown('**Unable to categorize image, please try another one! :)**)')
+    with cols[2]:
+        st.write(' ')
+        
 
     
